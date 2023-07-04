547e469c6f691da69c23e0843e4c079a94864c1c
Jokse78 <131283767+Jokse78@users.noreply.github.com>
2023-07-04 18:28:21 +0300

    java

---
+++ mano_failai/js/gmmd.js
@@ -0,0 +1,9 @@
+let metai = 1999;
+let menuo = 06;
+let diena = 15;
+console.log(suma=metai%10+menuo%10+diena%10)  //turi sudeti paskutinius skaicius, metu, menesio ir dienos
+
+let r = 15
+let pi = 3.14
+console.log(ilgis=2*r*pi)
+console.log(plotas=r**2*pi)
---
+++ mano_failai/js/kodas.js
@@ -0,0 +1,27 @@
+let x = 50;
+let y = 10;
+let suma = x + y;
+console.log(suma);
+
+x = 50;
+y = 10;
+let skirtumas = x - y;
+console.log(skirtumas);
+
+x = 50;
+y = 10;
+let daugyba = x * y;
+console.log(daugyba);
+
+x = 50;
+y = 10;
+let dalyba = x / y;
+console.log(dalyba);
+
+let b = 'Oksana Jakubauske';
+let c = 'Mano vardas yra ';
+console.log(c + b);
+
+
+
+
